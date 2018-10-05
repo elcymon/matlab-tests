c476a353d9797216ba9a33493659fdcbcf609fd0
elcymon <elcymon.coding@gmail.com>
2018-10-05 12:17:10 +0100

    Modified section of PulsFilter to loop through multiple Pitch and Trough

--- audio/gradientComputation.m
+++ audio/gradientComputation.m
@@ -5,56 +5,66 @@ xdata = xy(:,2);
 ydata = xy(:,end);
 t = xy(:,1);
 hz = 40;
-w = 10;
-step = 1*w;
+PulsFilterData = cell(11,5);
+PulsFilterData(1,:) = {'Pitch','Trough','+grad','-grad','%+ve'};
+PulsFilterData(2:end,1:2) = {10,0;10,10;10,20;10,30;...
+                             20,0;20,20;20,40;
+                             40,0;40,40;40,80};
+for i1 = 2:size(PulsFilterData,1)
+    w = PulsFilterData{i1,1};
+    step = PulsFilterData{i1,2};
 
-stArt = 1;
-eNd = w;
+    stArt = 1;
+    eNd = w;
 
-yPulsZeros = NaN(size(ydata));
-yPulsmeans = NaN(1,ceil(numel(ydata)/(step+w))-1);
-yPulsmax = NaN(1,ceil(numel(ydata)/(step+w))-1);
-xPuls = NaN(size(yPulsmeans));
-j = 1;
-i = 1;
-hold on
-iw = w;
-bluePuls = 0;
-redPuls = 0;
-Blue = 0;
-Red = 0;
-while eNd <= numel(ydata)
-    if isempty(ydata(stArt:eNd))
-        break;
-    end
-    yPulsZeros(stArt:eNd) = ydata(stArt:eNd);
-    yPulsmeans(j) = mean(ydata(stArt:eNd));
-    xPuls(j) = median(xdata(stArt:eNd));
-    
-    lineColor = '-b';
-    if j > 1
-        if yPulsmeans(j-1) < yPulsmeans(j)
-            lineColor = '-r';
-            redPuls = redPuls + 1;
-            Red = plot(xPuls(j-1:j),yPulsmeans(j-1:j),lineColor);
-        else
-            bluePuls = bluePuls + 1;
-            Blue = plot(xPuls(j-1:j),yPulsmeans(j-1:j),lineColor);
+    yPulsZeros = NaN(size(ydata));
+    yPulsmeans = NaN(1,ceil(numel(ydata)/(step+w))-1);
+    yPulsmax = NaN(1,ceil(numel(ydata)/(step+w))-1);
+    xPuls = NaN(size(yPulsmeans));
+    j = 1;
+    i = 1;
+%     hold on
+    iw = w;
+    bluePuls = 0;
+    redPuls = 0;
+    Blue = 0;
+    Red = 0;
+    while eNd <= numel(ydata)
+        if isempty(ydata(stArt:eNd))
+            break;
         end
+        yPulsZeros(stArt:eNd) = ydata(stArt:eNd);
+        yPulsmeans(j) = mean(ydata(stArt:eNd));
+        xPuls(j) = median(xdata(stArt:eNd));
+
+        lineColor = '-b';
+        if j > 1
+            if yPulsmeans(j-1) < yPulsmeans(j)
+                lineColor = '-r';
+                redPuls = redPuls + 1;
+%                 Red = plot(xPuls(j-1:j),yPulsmeans(j-1:j),lineColor);
+            else
+                bluePuls = bluePuls + 1;
+%                 Blue = plot(xPuls(j-1:j),yPulsmeans(j-1:j),lineColor);
+            end
+        end
+        yPulsmax(j) = max(ydata(stArt:eNd));
+        if eNd == numel(ydata)
+            break;
+        end
+        stArt = eNd + step + 1;
+        eNd = stArt + w;
+        if eNd > numel(ydata)
+            eNd = numel(ydata);
+        end
+        j = j + 1;
     end
-    yPulsmax(j) = max(ydata(stArt:eNd));
-    if eNd == numel(ydata)
-        break;
-    end
-    stArt = eNd + step + 1;
-    eNd = stArt + w;
-    if eNd > numel(ydata)
-        eNd = numel(ydata);
-    end
-    j = j + 1;
+    PulsFilterData{i1,3} = bluePuls;
+    PulsFilterData{i1,4} = redPuls;
+    PulsFilterData{i1,5} = bluePuls/(redPuls+bluePuls);
 end
-legend([Blue,Red],{num2str(bluePuls/(redPuls+bluePuls)),num2str(redPuls/(redPuls+bluePuls))})
-hold off
+%     legend([Blue,Red],{num2str(bluePuls/(redPuls+bluePuls)),num2str(redPuls/(redPuls+bluePuls))})
+%     hold off
 % figure(1)
 % 
 %     plot(xdata,yPulsZeros)
@@ -102,7 +112,6 @@ hold off
         if endPoint > numel(ydata)
             endPoint = numel(ydata);
         end
-        
     end
 %     hold off
 %     
