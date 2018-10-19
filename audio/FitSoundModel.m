d6c360d5502fb4f92439f7b900a7f3523f475d9c
elcymon <elcymon.coding@gmail.com>
2018-10-19 10:44:01 +0100

    FitSoundModel.m added

---
+++ audio/FitSoundModel.m
@@ -0,0 +1,25 @@
+function [pureSignal,meanError,stdError] = FitSoundModel(xdata,ydata,x0,Ae)
+% Function to fit sound data to a sound model represented by ndeed, which
+% is a negative exponential function.
+% All arguments are required:
+%   xdata is a vector representing distance
+%   ydata is a vector of equal size as xdata representing the corresponding
+%       sound intensity
+%   x0 is the starting point for lsqcurvefit function
+%   Ae is the ambient noise value 
+% Returns:
+%   pureSignal the fitted curve which represents the pure signal fit
+%   meanError the error between the pure signal and the ydata
+%   stdError the standard deviation between pure signal and ydata
+    ndeed = @(x,xdata) x(1)*(exp(-x(2) * xdata)) + Ae;
+    
+    x = lsqcurvefit(ndeed,x0,xdata,ydata);
+    
+    pureSignal = ndeed(x,xdata);
+    
+    yError = pureSignal - ydata;
+    meanError = mean(yError);
+    stdError = std(yError);
+    
+    
+end
